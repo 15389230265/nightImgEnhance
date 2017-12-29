1dd5bc4216a94215932b62a22ab26a504183c2f9
leiqi <503976309@qq.com>
2017-12-29 16:02:38 +0800

    fix

--- ssr.m
+++ ssr.m
@@ -1,10 +1,12 @@
 clear all;
 clc;
+close all;
 IMG = imread('img2.jpg');
 c=80;
 [wid,len,height]=size(IMG);
 sum=0;
 si = c*2+1;
+f=zeros(si);
 for i=0:si-1
     for j=0:si-1
         f(i+1,j+1)=exp(-((i-c)^2+(j-c)^2)/c^2);
@@ -12,6 +14,7 @@ for i=0:si-1
     end
 end
 k=1/sum;
+newIMG=zeros(size(IMG));
 for t=1:3
     w=IMG(:,:,t);
     w=double(w);
@@ -20,18 +23,44 @@ for t=1:3
     q=double(q);
     newIMG(:,:,t)=(log(w)-log(q));
     a=newIMG;
-    maxium=max(max(newIMG(:,:,t)));
-%     minium=min(min(newIMG(:,:,t)));
-    minium=0;
+end
+for t=1:3
+%---------------------------------------------------------------
+%     maxium=max(max(newIMG(:,:,t)));
+% %     minium=min(min(newIMG(:,:,t)));
+%     minium=0;
+%     for i=1:wid
+%         for j=1:len
+%             if newIMG(i,j,t)<0
+%                 newIMG(i,j,t)=0;
+%             end
+%             newIMG(i,j,t)=255*((newIMG(i,j,t)-minium)/(maxium-minium));
+%         end
+%     end
+% %     newIMG(:,:,t)=round(exp(newIMG(:,:,t).*5.54));
+%---------------------------------------------------------------
+    G=30;
+    B=-6;
+    I=double(IMG(:,:,t));
+    Ij=0;
+%     Ij=sum(sum(I));
     for i=1:wid
         for j=1:len
-            if newIMG(i,j,t)<0
-                newIMG(i,j,t)=0;
-            end
-            newIMG(i,j,t)=255*((newIMG(i,j,t)-minium)/(maxium-minium));
+            Ij=Ij+I(i,j);
         end
     end
-%     newIMG(:,:,t)=round(exp(newIMG(:,:,t).*5.54));
+    alpha=125;
+    beta=1;
+    Ci=zeros(size(IMG));
+    Ii=Ci;
+    for i=1:wid
+        for j=1:len
+%            newIMG(i,j,t)=(newIMG(i,j,t)-B).*G;
+           Ii(i,j,t)=I(i,j)./Ij;
+        end
+    end
+    Ci=(log((Ii.*alpha))).*beta;
+    newIMG=newIMG.*Ci;
 end
 subplot(1,2,1);imshow(IMG);title('原始图像');
 subplot(1,2,2);imshow(newIMG);title('SSR增强后的图像');
